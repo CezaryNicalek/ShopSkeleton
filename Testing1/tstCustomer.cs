2cad1f6f59eb6ea32f0079c4bc364ea0898620fc
CezaryNicalek <78745192+CezaryNicalek@users.noreply.github.com>
2021-02-21 18:12:35 +0000

    Added testing and class for customer menagement

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
